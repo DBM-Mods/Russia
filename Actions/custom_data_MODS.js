1d9f7c1198d30a107c3bc7ab8a20f1515bcf6398
LIK (DANIL) <61782941+btcactus@users.noreply.github.com>
2022-10-03 09:43:37 +0500

    Переведено с Португальского на Русский язык

--- Actions/custom_data_MODS.js
+++ Actions/custom_data_MODS.js
@@ -25,7 +25,7 @@ module.exports = {
 
   html(isEvent, data) {
     return `
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.1</div>
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Версия 0.1</div>
     <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
 
     <tab-system style="margin-top: 20px;">
