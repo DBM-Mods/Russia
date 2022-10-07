2803723ea823fccb66fb5a4b4774081ab13c3345
LIK (DANIL) <61782941+btcactus@users.noreply.github.com>
2022-10-07 13:23:19 +0500

    Переведено с Португальского на Русский язык

---
+++ Actions/go_to_anchor_MODS.js
@@ -0,0 +1,47 @@
+module.exports = {
+  name: 'Go To Action Anchor MOD',
+  section: 'Other Stuff',
+  meta: {
+    version: '2.1.6',
+    preciseCheck: true,
+    author: '[XinXyla - 172782058396057602]',
+    authorUrl: 'https://github.com/DBM-Mods/Russia',
+    downloadURL: 'https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip',
+  },
+  
+  subtitle(data) {
+    return data.description
+    ? `<font color="${data.color}">Якорь: ${data.anchorName} | ${data.description}</font>`
+    : `<font color="${data.color}">Якорь: ${data.anchorName}`},
+
+  fields: ['description', 'anchorName', 'color'],
+
+  html() {
+    return `
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Версия 0.2</div>
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+<div style="float: left; width: 74%;">
+<span class="dbminputlabel">Якорь</span><br>
+  <input type="text" class="round" id="anchorName"><br>
+</div>
+<div style="float: right; width: 24%;text-align:center">
+<span class="dbminputlabel">Описание Цвет</span><br>
+  <input type="color" id="color" class="round"><br>
+</div>
+<div style="float: left; width: 98%;">
+<span class="dbminputlabel">Описание</span><br>
+  <input type="text" class="round" id="description"><br>
+</div>`;
+  },
+
+  init() {},
+
+
+  action(cache) {
+    const data = cache.actions[cache.index];
+    const anchorName = this.evalMessage(data.anchorName, cache);
+    cache.goToAnchor(anchorName);
+  },
+
+  mod() {},
+};
