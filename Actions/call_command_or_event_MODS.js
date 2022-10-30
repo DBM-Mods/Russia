f3787a3766f2de9e18d603f796556968992a83d4
LIK (DANIL) <61782941+btcactus@users.noreply.github.com>
2022-10-30 17:23:13 +0500

    Обновлено до 0.4 версии

--- Actions/call_command_or_event_MODS.js
+++ Actions/call_command_or_event_MODS.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Call Command/Event MOD',
   section: 'Other Stuff',
   meta: {
-    version: '2.1.5',
+    version: '2.1.6',
     preciseCheck: true,
     author: '[XinXyla - 172782058396057602]',
     authorUrl: 'https://github.com/DBM-Mods/Russia',
@@ -11,13 +11,16 @@ module.exports = {
 
   subtitle(data) {
     let source;
+    if (parseInt(data.sourcetype, 10) === 3) {
+      source = 'Та же команда/событие';
+    }
     if (parseInt(data.sourcetype, 10) === 2) {
       source = 'Имя команды: ' + data.source3.toString();
     }
     if (parseInt(data.sourcetype, 10) === 1) {
       source = 'Идентификатор (ID) команды: ' +data.source2.toString();
     } 
-    if (parseInt(data.sourcetype, 10) === 0) {
+    if (parseInt(data.sourcetype, 10) === 0 || data.sourcetype == undefined) {
       source = 'Список > команда: ' +data.source.toString();
     }
     return `${source}`;
@@ -27,32 +30,33 @@ module.exports = {
 
   html() {
     return `
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Версия 0.2</div>
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Версия 0.4</div>
     <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
 <div style="float: left; width: 100%; padding-top: 20px;">
-<span class="dbminputlabel">Тип шрифта</span><br>
+<span class="dbminputlabel">Тип выбора</span><br>
   <select id="sourcetype" class="round" onchange="glob.onChange1(this)">
     <option value="0" selected>Выбрать из списка</option>
-    <option value="1">Введите идентификатор команды/события</option>
-    <option value="2">Введите имя команды/события</option>
+    <option value="1">Введите команду/идентификатор (ID) события</option>
+    <option value="2">Введите имя команды/событие</option>
+    <option value="3">Та же команда/событие</option>
   </select>
 </div>
 <div id="info1"; style="float: left; width: 100%; padding-top: 20px; display: none;">
 <span class="dbminputlabel">Команда/событие</span><br>
   <select id="source" class="round">
-    <optgroup id="commands" label="Commands"></optgroup>
-    <optgroup id="events" label="Events"></optgroup>
+    <optgroup id="commands" label="Команды"></optgroup>
+    <optgroup id="events" label="События"></optgroup>
   </select>
 </div>
 <div id="info2" style="float: left; width: 100%; padding-top: 20px;">
-<span class="dbminputlabel">ID команды/события</span><br>
+<span class="dbminputlabel">Идентификатор (ID) команды/события</span><br>
   <input id="source2" class="round" type="text" placeholder="">
 </div>
 <div id="info3" style="float: left; width: 100%; padding-top: 20px;">
-<span class="dbminputlabel">Название команды/события</span><br>
+<span class="dbminputlabel">Имя команды / события</span><br>
   <input id="source3" class="round" type="text" placeholder="">
 </div>
-<div style="float: left; width: 85%; padding-top: 20px;">
+<div style="float: left; width: 100%; padding-top: 20px;">
 <span class="dbminputlabel">Тип вызова</span><br>
   <select id="type" class="round">
   <option value="true" selected>Дождаться завершения</option>
@@ -104,6 +108,11 @@ module.exports = {
           info2.style.display = 'none';
           info3.style.display = null;
           break;
+        case 3:
+          info1.style.display = 'none';
+          info2.style.display = 'none';
+          info3.style.display = 'none';
+          break;
         default:
           break;
       }
@@ -115,40 +124,64 @@ module.exports = {
   action(cache) {
     const data = cache.actions[cache.index];
     const { Files } = this.getDBM();
+    source = this.evalMessage(data.source, cache)
+    source2 = this.evalMessage(data.source2, cache)
+    source3 = this.evalMessage(data.source3, cache)
 
     let id;
-    if (parseInt(data.sourcetype, 10) === 1) {
-      id = this.evalMessage(data.source2, cache);
-    } else {
-      id = data.source;
+
+    if (data.sourcetype == "0" || data.sourcetype == undefined) {
+      id = source
     }
-    if (!id) return console.log('Введите КОД строки/мероприятия!');
+
+    if (data.sourcetype == "1") {
+      id = source2
+      if (!id) return console.log('Введите идентификатор (ID) команды/событие!')
+    }
 
     let name;
-    if (parseInt(data.sourcetype, 10) === 2) {
-      name = this.evalMessage(data.source3, cache);
-    } else {
-      name = data.source;
-    }
-    if (!name) return console.log('Введите название команды/события!');
 
+    if (data.sourcetype == "2") {
+      name = source3
+      if (!name) return console.log('Введите имя команды / события!')
+    }
+
+    if (data.sourcetype == "3") {
+    const jp = this.getMods().require('jsonpath');
+    var interaction = cache.interaction;
+
+    if(interaction){
+      command = jp.query(
+        this.getDBM().Files.data.commands,
+        `$..[?(@.name=="${interaction.commandName}")]`,
+      );
+      } else {
+        command = jp.query(
+          this.getDBM().Files.data.commands,
+          `$..[?(@.name=="${cache.msg.content
+            .slice(this.getDBM().Files.data.settings.tag.length || cache.server.tag.length)
+            .split(/ +/)
+            .shift()}")]`,
+        )
+        }
+        
+        idsave = jp.query(command, '$.._id')
+        id = idsave.toString()
+      }
+    
 
     let actions;
+
     const allData = Files.data.commands.concat(Files.data.events);
     for (let i = 0; i < allData.length; i++) {
-      if (parseInt(data.sourcetype, 10) === 0) {
+
+      if (data.sourcetype == "0" || data.sourcetype == "1" || data.sourcetype == "3" || data.sourcetype == undefined) {
         if (allData[i] && allData[i]._id === id) {
           actions = allData[i].actions;
           break;
         }
       }
-      if (parseInt(data.sourcetype, 10) === 1) {
-        if (allData[i] && allData[i]._id === id) {
-          actions = allData[i].actions;
-          break;
-        }
-      }
-      if (parseInt(data.sourcetype, 10) === 2) {
+      if (data.sourcetype == "2") {
         if (allData[i] && allData[i].name === name) {
           actions = allData[i].actions;
           break;
