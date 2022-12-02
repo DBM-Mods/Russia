e53ade6f74a071cf05371326c1737fcefc4483ff
LIK (DANIL) <61782941+btcactus@users.noreply.github.com>
2022-12-02 22:11:46 +0500

    Обновлено действие до 0.5 версии

--- Actions/action_anchor_MODS.js
+++ Actions/action_anchor_MODS.js
@@ -10,28 +10,34 @@ module.exports = {
   },
   
   subtitle(data) {
-    return data.description
-      ? `<font color="${data.color}">Якорь: ${data.anchorName} | ${data.description}</font>`
-      : `<font color="${data.color}">Якорь: ${data.anchorName}`},
+    if(data.descriptionx == true){
+      desccor = data.color
+      } else {
+        desccor = 'none'
+      }
 
-  fields: ['anchorName', 'color', 'description', 'config', 'actions'],
+    return data.description
+      ? `<font style="color:${desccor}">Якорь: ${data.anchorName} | ${data.description}</font>`
+      : `<font style="color:${desccor}">Якорь: ${data.anchorName}</font>`},
+
+  fields: ['anchorName', 'color', 'description', 'config', 'actions',"descriptionx"],
 
   html() {
     return `
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Версия 0.3</div>
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip">Обновить</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Russia">Версия 0.5</div>
 <tab-system>
 
-    <tab label="Якорь" icon="anchor">
+    <tab label="Âncora" icon="anchor">
     <div style="width: 100%; padding:10px 5px;height: calc(100vh - 210px);overflow:auto">
 
 <div style="float:left;width:75%">
-<span class="dbminputlabel">Якоря название</span><br>
+<span class="dbminputlabel">Имя якоря</span><br>
   <input type="text" class="round" id="anchorName"><br>
 </div>
 
 <div style="float: right; width: 24%;text-align:center">
-<span class="dbminputlabel">Описание Цвет</span><br><input type="color" class="round" id="color">
+<div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Цвет (вкл)"></dbm-checkbox></div><br><input type="color" class="round" id="color">
 </div>
 
   <div style="float: left; width: 100%;">
@@ -41,14 +47,14 @@ module.exports = {
 
   </div>
   </tab>
-  <tab label="Действия / конфигурация" icon="list layout">
+  <tab label="Действия / конфиг" icon="list layout">
   <div style="width: 100%; padding:10px 5px;height: calc(100vh - 210px);overflow:auto">
-  <span class="dbminputlabel">Настройки</span>
+  <span class="dbminputlabel">Использование</span>
   <select id="config" class="round" onchange="glob.onComparisonChanged(this)">
-    <option value="0" selected>Продолжать действия</option>
-    <option value="1" selected>Остановить действия</option>
-    <option value="2">Используйте следующие действия и продолжайте</option>
-    <option value="3">Используйте только следующие действия и остановитесь</option>
+    <option value="0" selected>Продолжить действия</option>
+    <option value="1">Остановить действия</option>
+    <option value="2">Использовать следующие действия (из спика ниже!) и продолжить</option>
+    <option value="3">Использовать только следующие действия (из спика ниже!) и остановится</option>
   </select>
 <br>
 <div id="containerxin">
@@ -56,6 +62,13 @@ module.exports = {
   </div>
   </div>
   </tab-system>
+
+  <style>
+
+.dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
+.dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
+
+</style>
   `;
   },
 
@@ -63,7 +76,7 @@ module.exports = {
     const { glob, document } = this;
 
     glob.onComparisonChanged = function (event) {
-      if (event.value == 1 || event.value == 2 || event.value == 3) {
+      if (event.value == 2 || event.value == 3) {
         document.getElementById("containerxin").style.display = "block";
       }
       else {
@@ -73,6 +86,19 @@ module.exports = {
 
     glob.onComparisonChanged(document.getElementById("config"));
 
+    const xinelaslinks = document.getElementsByClassName('xinelaslink');
+    for (let x = 0; x < xinelaslinks.length; x++) {
+      const xinelaslink = xinelaslinks[x];
+      const url = xinelaslink.getAttribute('data-url');
+      if (url) {
+       xinelaslink.setAttribute('title', url);
+       xinelaslink.addEventListener('click', (e) => {
+          e.stopImmediatePropagation();
+          console.log(`Запуск URL: [${url}] В вашем браузере по умолчанию.`);
+          require('child_process').execSync(`start ${url}`);
+        });
+      }
+    }
 
   },
 
