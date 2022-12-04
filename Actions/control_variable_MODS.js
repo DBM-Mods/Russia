1630fd45ea7993cb87116d4fb75b9159fba775b5
LIK (DANIL) <61782941+btcactus@users.noreply.github.com>
2022-12-04 09:34:46 +0500

    Переведено действие с Португальского на Русский язык

---
+++ Actions/control_variable_MODS.js
@@ -0,0 +1,138 @@
+module.exports = {
+
+  name: "Control Variable MOD",
+  section: "Other Stuff",
+  meta: {
+    version: '2.1.6',
+    preciseCheck: true,
+    author: '[XinXyla - 172782058396057602]',
+    authorUrl: 'https://github.com/DBM-Mods/Russia',
+    downloadURL: 'https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip',
+  },
+
+  subtitle(data, presets) {
+
+    if(data.descriptionx == true){
+      desccor = data.descriptioncolor
+      } else {
+        desccor = 'none'
+      }
+
+    const storage = presets.variables;
+
+    return data.description
+    ? `<font style="color:${desccor}">${data.description}</font>`
+    : `<font style="color:${desccor}">${storage[parseInt(data.storage, 10)]} (${data.varName}) ${data.changeType === "1" ? "+=" : "="} ${
+      data.value
+    }</font>`
+  },
+
+
+  variableStorage(data, varType) {
+    const type = parseInt(data.storage, 10);
+    if (type !== varType) return;
+    return [data.varName, "Unknown Type"];
+  },
+
+
+  fields: ["storage", "varName", "changeType", "value","descriptioncolor","description","descriptionx"],
+
+
+  html(isEvent, data) {
+    return `
+    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip">Обновить</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Russia">Версия 0.3</div>
+
+    <div style="width: 100%; padding:5px 5px;height: calc(100vh - 160px);overflow:auto">
+
+    <div id="flutuador" style="padding:0px 0px 15px 0px">
+<table style="width:100%;"><tr>
+<td><span class="dbminputlabel">Описание действия</span><br><input type="text" class="round" id="description" placeholder="Оставьте пустым, чтобы не использовалось!"></td>
+<td style="padding:0px 0px 0px 10px;width:70px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Цвет (вкл)"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
+</tr></table>
+</div>
+
+<store-in-variable dropdownLabel="Хранится в" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></store-in-variable>
+
+<br><br><br>
+
+<div style="padding-top: 8px;">
+	<div style="float: left; width: 35%;">
+		<span class="dbminputlabel">Тип управления</span><br>
+		<select id="changeType" class="round">
+			<option value="0" selected>Изменить значение</option>
+			<option value="1">Добавить значение</option>
+		</select>
+	</div></div>
+
+  <br><br><br>
+
+	<div style="float: left; width: 100%;">
+		<span class="dbminputlabel">Значение</span><br>
+    <textarea id="value" rows="6" class="round" name="is-eval" style="width:100%"></textarea><br>
+	</div>
+
+  </div>
+
+<style>
+
+.dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
+.dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
+
+</style>
+`;
+  },
+
+
+
+  init() {
+    const { glob, document } = this;
+
+    const xinelaslinks = document.getElementsByClassName('xinelaslink');
+    for (let x = 0; x < xinelaslinks.length; x++) {
+      const xinelaslink = xinelaslinks[x];
+      const url = xinelaslink.getAttribute('data-url');
+      if (url) {
+       xinelaslink.setAttribute('title', url);
+       xinelaslink.addEventListener('click', (e) => {
+          e.stopImmediatePropagation();
+          console.log(`Launching URL: [${url}] in your default browser.`);
+          require('child_process').execSync(`start ${url}`);
+        });
+      }
+    }
+
+  },
+
+
+  action(cache) {
+    const data = cache.actions[cache.index];
+    const type = parseInt(data.storage, 10);
+    const varName = this.evalMessage(data.varName, cache);
+    const storage = this.getVariable(type, varName, cache);
+    const isAdd = data.changeType === "1";
+    let val = this.evalMessage(data.value, cache);
+    try {
+      val = this.eval(val, cache);
+    } catch (e) {
+      this.displayError(data, cache, e);
+    }
+    if (val !== undefined) {
+      if (isAdd) {
+        let result;
+        if (storage === undefined) {
+          result = val;
+        } else {
+          result = storage + val;
+        }
+        this.storeValue(result, type, varName, cache);
+      } else {
+        this.storeValue(val, type, varName, cache);
+      }
+    }
+    this.callNextAction(cache);
+  },
+
+
+  mod() {},
+};
