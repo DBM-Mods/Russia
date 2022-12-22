d2a433a1b0339100665fa6f42f636f4b6c71c67e
LIK (DANIL) <61782941+likDanil@users.noreply.github.com>
2022-12-22 15:00:40 +0500

    meta update

--- Actions/delete_file_MODS.js
+++ Actions/delete_file_MODS.js
@@ -5,8 +5,8 @@ module.exports = {
     version: '2.1.6',
     preciseCheck: true,
     author: '[XinXyla - 172782058396057602]',
-    authorUrl: 'https://github.com/DBM-Mods/Portugues',
-    downloadURL: 'https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip',
+    authorUrl: 'https://github.com/DBM-Mods/Russia',
+    downloadURL: 'https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip',
   },
 
 
