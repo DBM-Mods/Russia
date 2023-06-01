e260b4850d7f4feab0ba16991e8e079ae5461835
Juliana <135098148+jju1iana@users.noreply.github.com>
2023-06-01 23:33:00 +0600

    Convert list to text MODS v0.4

--- Actions/convert_list_to_text_MODS.js
+++ Actions/convert_list_to_text_MODS.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: "Convert List to Text MOD",
+  name: "Convert list to text MOD",
   section: "Lists and Loops",
   meta: {
     version: '2.1.7',
@@ -21,26 +21,46 @@ module.exports = {
 
     return data.description
       ? `<font style="color:${desccor}">${data.description}</font>`
-      : `<font style="color:${desccor}">Конвертировать ${list[parseInt(data.list, 10)]} для текста</font>`
+      : `<font style="color:${desccor}">Конвертер ${list[parseInt(data.list, 10)]} для текста</font>`
   },
 
 
   variableStorage(data, varType) {
-    const type = parseInt(data.storage, 10);
-    if (type !== varType) return;
-    return [data.varName2, "Text"];
+    let vars = [];
+
+    const type1 = parseInt(data.storagept, 10)
+    const varName1 = data.varNamept;
+
+    if (type1 == varType && varName1) {
+      vars.push(varName1);
+      vars.push("Номер");
+    }
+    const type2 = parseInt(data.storage, 10)
+    const varName2 = data.varName2;
+
+    if (type2 == varType && varName2) {
+      vars.push(varName2);
+      vars.push("Текст");
+    }
+
+
+    if (vars.length > 0) {
+      return vars;
+    }
+
+
   },
 
 
-  fields: ["list", "varName", "numero", "start", "start2", "start3", "multi", "end", "end2", "end3", "posicao", "storage", "varName2", "itens", "descriptioncolor", "description", "descriptionx", "limite"],
+  fields: ["list", "varName", "numero", "istart", "start", "istart2", "start2", "start3", "multi", "end", "end2", "end3", "posicao", "storage", "varName2", "itens", "descriptioncolor", "description", "descriptionx", "limite", "pagina", "storagept", "varNamept", "endeval", "checkend", "endlib"],
 
   html(isEvent, data) {
     return `
-    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip">Обновить</div>
-    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Russia">Версия 0.3</div>
+    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip">Обновление</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Russia">Версия 0.4</div>
 
     <tab-system style="margin-top: 0;">
-		<tab label="Конвертер" icon="align left">
+		<tab label="Converter" icon="align left">
     <div style="width: 100%; padding:10px 5px;height: calc(100vh - 210px);overflow:auto">
 
     <table><tr><td class="col">
@@ -57,7 +77,7 @@ module.exports = {
  
 
   <table><tr><td class="col">
-<span class="dbminputlabel">Результат в</span><br>
+<span class="dbminputlabel">Результат</span><br>
 		<select id="storage" class="round">
 		${data.variables[1]}
 		</select><br>
@@ -69,38 +89,59 @@ module.exports = {
 
   </div>
     </tab>
-    <tab label="Все элементы" icon="align left">
+    <tab label="Все пункты" icon="align left">
 
 <div style="width: 100%; padding: 10px;height: calc(100vh - 210px);overflow:auto">
 
-<dbm-checkbox id="numero" label="Показать номер позиции в каждом элементе"></dbm-checkbox>
+<span class="dbminputlabel">Начало каждого пункта</span><br>
+<textarea id="istart" rows="3" style="width: 100%; white-space: nowrap; resize: yes;"></textarea>
+<br>
+<dbm-checkbox id="numero" label="Укажите номер позиции на каждом элементе"></dbm-checkbox>
 <br>
 	
-		<span class="dbminputlabel">Начало каждого элемента</span><br>
+		<span class="dbminputlabel">Середина каждого пункта</span><br>
     <textarea id="start" rows="3" style="width: 100%; white-space: nowrap; resize: yes;"></textarea>
 	
 <br>
 	
-		<span class="dbminputlabel">Конец каждого элемента</span><br>
-    <textarea id="end" rows="3" style="width: 100%; white-space: nowrap; resize: yes;" value="\\n"></textarea><br>
+      
+  <span class="dbminputlabel" name="xinelas">Конец каждого пункта</span> <div style="float:right;margin-top:-5px"><dbm-checkbox id="checkend" onchange="glob.onChange3(this)" label="EVAL ITEM"></dbm-checkbox></div><br>
+  <div id="valor"><textarea id="end" rows="3" class="round" style="width:100%; white-space: nowrap; resize: yes;"></textarea></div>
+  <div style="display:none" id="valoreval"><textarea id="endeval" rows="3" name="is-eval" class="round" style="width:100%; white-space: nowrap; resize: yes;"></textarea>
+<br>
+
+  <dbm-checkbox id="endlib" label="Не показывайте стоимость каждого предмета"></dbm-checkbox>
+
+  <br>
+  Use <span style="background:rgba(0,0,0,0.8);color:lime;padding:2px;">list[i]</span> для сбора информации из элемента списка, если он находится в формате JSON, пример <span style="background:rgba(0,0,0,0.8);color:lime;padding:2px;">list[i].user.username</span> da lista "Члены сервера"
+  <br>Use <span style="background:rgba(0,0,0,0.8);color:lime;padding:2px;">\\n</span> para pular de linha
+  
+  </div>
+  <br>
+
 
 </div>
 </tab>
-    <tab label="Элемент" icon="align left">
+    <tab label="Пункты" icon="align left">
     <div style="width: 100%; padding: 10px;height: calc(100vh - 210px);overflow:auto">
 
 
-  <span class="dbminputlabel">Каждый * элементы</span><br>
+  <span class="dbminputlabel">С каждым * пунктом</span><br>
 <input id="multi" class="round" type="text" value="0">
+
+<br>
+
+		<span class="dbminputlabel">Добавить без начала</span><br>
+    <textarea id="istart2" rows="3" style="width: 100%; white-space: nowrap; resize: yes;"></textarea>
 		
 <br>
 
-		<span class="dbminputlabel">Добавить в начало</span><br>
+		<span class="dbminputlabel">Добавить в середину</span><br>
     <textarea id="start2" rows="3" style="width: 100%; white-space: nowrap; resize: yes;"></textarea>
 	
 <br>
 	
-		<span class="dbminputlabel">Добавить в конец</span><br>
+		<span class="dbminputlabel">Добавить не окончательный</span><br>
     <textarea id="end2" rows="3" style="width: 100%; white-space: nowrap; resize: yes;" value="\\n"></textarea><br>
 
 </div>
@@ -108,46 +149,46 @@ module.exports = {
 
 </tab>
 
-<tab label="Редактировать элементы" icon="align left">
-<dialog-list id="itens" fields='["posicao","start3","end3", "val1", "val2", "comparar", "formula"]' dialogTitle="Редактор элементов" dialogWidth="500" dialogHeight="510" listLabel="Элименты" listStyle="height: calc(100vh - 250px);" itemName="Editar item" itemCols="1" itemHeight="40px;" itemTextFunction="data.formula + ' / Позиция: ' + data.posicao" itemStyle="text-align: left; line-height: 40px;">
+<tab label="Редактировать пункты" icon="align left">
+<dialog-list id="itens" fields='["posicao","start3","end3", "val1", "val2", "comparar", "formula"]' dialogTitle="Редактировать пункты" dialogWidth="500" dialogHeight="510" listLabel="Пункты" listStyle="height: calc(100vh - 250px);" itemName="Редактирование" itemCols="1" itemHeight="40px;" itemTextFunction="data.formula + ' / Позиция: ' + data.posicao" itemStyle="text-align: left; line-height: 40px;">
 <div style="padding: 16px;background:rgba(0,0,0,0.3)">
                  <span class="dbminputlabel">Редактировать</span><br>
                  <select id="formula" class="round">
-                 <option value="Всегда" selected>Всегда редактировать элемент</option>
-                 <option value="False">Редактировать, если получено значение False</option>
-                 <option value="True">Редактировать, если получено значение True</option>
+                 <option value="Editar" selected>Всегда редактируйте элемент</option>
+                 <option value="Falso">Редактирование только в том случае, если компаратор ложен</option>
+                 <option value="Verdadeiro">Редактирование только в том случае, если компаратор истинен</option>
                </select>
                <br>
                   <table style="width:100%"><tr><td class="col">
-                    <span class="dbminputlabel">Значение А</span><br>
+                    <span class="dbminputlabel">Valor A</span><br>
                     <input id="val1" class="round" type="text">
                     </td>
                     <td class="col">
-                    <span class="dbminputlabel">Сравнения</span><br>
+                    <span class="dbminputlabel">Сравнить</span><br>
                     <select id="comparar" class="round">
-                    <option value="0">Значение A - существует</option>
-                    <option value="1" selected>Равно</option>
+                    <option value="0">Значение A - Существующее</option>
+                    <option value="1" selected>Равный</option>
                     <option value="2">Точно так же</option>
-                    <option value="3">Меньше чем</option>
+                    <option value="3">Менее</option>
                     <option value="13">Меньше или равно</option>
-                    <option value="4">Больше тогда</option>
+                    <option value="4">Больше, чем</option>
                     <option value="12">Больше или равно</option>
-                    <option value="5">Включать</option>
-                    <option value="6">Соответствует регулярному выражению</option>
-                    <option value="14">Соответствует полному регулярному выражению</option>
-                    <option value="7">Длина длиннее, чем</option>
-                    <option value="8">Длина ниже, чем</option>
+                    <option value="5">Включает в себя</option>
+                    <option value="6">Соответствует регексу</option>
+                    <option value="14">Совпадает с полным регексом</option>
+                    <option value="7">Длина больше, чем</option>
+                    <option value="8">Длина меньше, чем</option>
                     <option value="9">Длина равна</option>
-                    <option value="10">Начинается с</option>
-                    <option value="11">Заканчивается</option>
-                    <option value="16">Значение А имеет акценты?</option>
-                    <option value="17">Включает слова  ["a" , "b" , "c"]</option>
-                    <option value="18">Равны словам  ["a" , "b" , "c"]</option>
-                    <option value="19">Значение A - это чётное число?</option>
-                    <option value="20">Значение A - это нечетное число?</option>
-                    <option value="21">Значение A является числом?</option>
+                    <option value="10">Она начинается с</option>
+                    <option value="11">Заканчиается на</option>
+                    <option value="16">Имеет ли значение A акценты?</option>
+                    <option value="17">Включает в себя слова ["a" , "b" , "c"]</option>
+                    <option value="18">Это просто как слова ["a" , "b" , "c"]</option>
+                    <option value="19">Является ли значение A четным числом?</option>
+                    <option value="20">Является ли значение A нечетным числом?</option>
+                    <option value="21">Является ли значение A числом?</option>
                     <option value="24">Значение A - это текст?</option>
-                    <option value="23">Значение A - это URL -адрес изображения?</option>
+                    <option value="23">Является ли значение A URL-адресом изображения?</option>
                     <option value="25">Значение A - это URL?</option>
                   </select>
                    </td>
@@ -165,30 +206,42 @@ module.exports = {
   
 <br>
 
-  <span class="dbminputlabel">Добавить в начало</span><br>
+  <span class="dbminputlabel">Добавить без начала</span><br>
   <textarea id="start3" rows="3" style="width: 100%; white-space: nowrap; resize: yes;"></textarea>
 
 <br>
 
-  <span class="dbminputlabel">Добавить в конце</span><br>
+  <span class="dbminputlabel">Добавить в середину</span><br>
   <textarea id="end3" rows="3" style="width: 100%; white-space: nowrap; resize: yes;" value="\\n"></textarea><br>
 
 </div>
 </dialog-list>
 </tab>
-<tab label="Конфиг" icon="cogs">
+<tab label="Config" icon="cogs">
 <div style="width: 100%; padding:10px 5px;height: calc(100vh - 210px);overflow:auto">
 
 
 <div style="padding-bottom: 12px;padding-top: 12px">
 <table style="width:100%;"><tr>
-<td><span class="dbminputlabel">Описание действия</span><br><input type="text" class="round" id="description" placeholder="Оставьте пустым, чтобы не использовалось!"></td>
-<td style="padding:0px 0px 0px 10px;width:55px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Цвет (вкл)"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
+<td><span class="dbminputlabel">Описание действий</span><br><input type="text" class="round" id="description" placeholder="Оставьте пустым, чтобы удалить"></td>
+<td style="padding:0px 0px 0px 10px;width:55px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Цвет"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
 </tr></table>
 </div>
 
-<span class="dbminputlabel">Лимит элементов</span><br><input type="text" class="round" id="limite" placeholder="Оставьте пустым, чтобы отключить">
-
+<span class="dbminputlabel">Ограничение количества элементов на странице</span><br><input type="text" class="round" id="limite" placeholder="Deixe vazio para desativar">
+<br>
+<span class="dbminputlabel">Страница</span><br><input type="text" class="round" id="pagina" value="1" placeholder="Deixe vazio para 1">
+<br>
+<div style="float: left; width: 35%;">
+	<span class="dbminputlabel">Всего страниц</span><br>
+		<select id="storagept" class="round" onchange="glob.variableChange(this, 'varNameContainer3')">
+		${data.variables[0]}
+		</select>
+	</div>
+	<div id="varNameContainer3" style="float: right; display: none; width: 60%;">
+	<span class="dbminputlabel">Имя переменной</span><br>
+		<input id="varNamept" class="round" type="text">
+	</div>
 
 </div>
 </tab>
@@ -219,11 +272,44 @@ table{width:100%}
         xinelaslink.setAttribute('title', url);
         xinelaslink.addEventListener('click', (e) => {
           e.stopImmediatePropagation();
-          console.log(`Запуск URL: [${url}] В вашем браузере по умолчанию.`);
+          console.log(`Launching URL: [${url}] in your default browser.`);
           require('child_process').execSync(`start ${url}`);
         });
       }
     }
+
+    glob.onChange2 = function (event) {
+      const value = parseInt(event.value, 10);
+      const dom = document.getElementById("varNameContainer3");
+      if (value == 0) {
+        dom.style.display = "none";
+      } else {
+        dom.style.display = null;
+      }
+    };
+
+    glob.onChange2(document.getElementById("storagept"));
+
+    glob.onChange3 = function (event) {
+      if (event.value == true) {
+        document.getElementById("valoreval").style.display = null;
+        document.getElementById("valor").style.display = "none";
+        document.querySelector("[name='xinelas']").innerText = (`Конец каждого пункта / EVAL ITEM`);
+      }
+      if (event.value == false) {
+        document.getElementById("valoreval").style.display = "none";
+        document.getElementById("valor").style.display = null;
+        document.querySelector("[name='xinelas']").innerText = (`Конец каждого пункта`);
+      }
+    };
+
+    glob.onChange3(document.getElementById("checkend"));
+
+    ;
+
+
+
+
 
   },
 
@@ -232,7 +318,23 @@ table{width:100%}
     var list = await this.getListFromData(data.list, data.varName, cache);
 
     const start = this.evalMessage(data.start, cache).replace("\\n", "\n");
+    const istart = this.evalMessage(data.istart, cache).replace("\\n", "\n");
     const end = this.evalMessage(data.end, cache).replace("\\n", "\n");
+
+    var totaldeitens = list.length
+
+    var irseguir = 0
+
+    var pagina = this.evalMessage(data.pagina, cache)
+    if (pagina == "" || pagina == "undefined" || pagina == undefined || pagina == null) {
+      pagina = 1
+    } else {
+      pagina = parseFloat(this.evalMessage(data.pagina, cache))
+      if (pagina > 0) { } else { pagina = 1 }
+      list = list.slice(0, limite)
+    }
+
+  
 
     const multi = parseFloat(this.evalMessage(data.multi, cache))
     var limite = this.evalMessage(data.limite, cache)
@@ -241,9 +343,28 @@ table{width:100%}
       limiteoff = "off"
     } else {
       limite = parseFloat(this.evalMessage(data.limite, cache))
-      list = list.slice(0, limite)
+
+      if (limite > 0) { } else { limite = 10 }
+
+      var paginatotal = Math.ceil(totaldeitens / limite)
+
+      const varNamept = this.evalMessage(data.varNamept, cache);
+      const storagept = parseInt(data.storagept, 10);
+
+      this.storeValue(paginatotal, storagept, varNamept, cache);
+
+      if (pagina > paginatotal) { pagina = paginatotal }
+      if (pagina <= 0) { pagina = 1 }
+
+      var irseguir = Math.ceil(pagina * limite - limite)
+
+      const indiceFim = irseguir + limite;
+
+
+      list = list.slice(irseguir, indiceFim)
     }
 
+    const istart2 = this.evalMessage(data.istart2, cache).replace("\\n", "\n");
     const start2 = this.evalMessage(data.start2, cache).replace("\\n", "\n");
     const end2 = this.evalMessage(data.end2, cache).replace("\\n", "\n");
 
@@ -255,16 +376,21 @@ table{width:100%}
     for (let i = 0; i <= (list.length - 1); i++) {
 
       resultitem = ""
+      resultitem2 = ""
 
       if (data.numero == true) {
-        resultitem = parseFloat(parseFloat([i]) + 1) + resultitem
+        resultitem = parseFloat(parseFloat([i + irseguir]) + 1) + resultitem
+        resultitem2 = parseFloat(parseFloat([i + irseguir]) + 1) + resultitem2
       }
+
 
       if (acont == [i] && multi > 0) {
         acont = acont + multi
-        resultitem = resultitem + start + start2 + String(list[i]) + end2
+        resultitem = istart2 + istart + resultitem + start + start2 + String(list[i]) + end2
+        resultitem2 = istart2 + istart + resultitem2 + start + start2 + end2
       } else {
-        resultitem = resultitem + start + String(list[i]);
+        resultitem = istart + resultitem + start + String(list[i]);
+        resultitem2 = istart + resultitem2 + start
       }
 
 
@@ -272,12 +398,12 @@ table{width:100%}
 
         const posicao = parseFloat(this.evalMessage(data.itens[ix].posicao, cache))
 
-        if (posicao == [i]) {
+        if (posicao == (i + irseguir)) {
           val1 = this.evalMessage(data.itens[ix].val1, cache);
           val2 = this.evalMessage(data.itens[ix].val2, cache);
           resulty = true;
 
-          if (data.itens[i].formula == "False" || data.itens[i].formula == "True") {
+          if (data.itens[ix].formula == "Falso" || data.itens[ix].formula == "Verdadeiro") {
             const compare = parseInt(data.itens[ix].comparar, 10);
             switch (compare) {
               case 0:
@@ -361,7 +487,7 @@ table{width:100%}
             }
           }
 
-          if (data.itens[ix].formula == "False") {
+          if (data.itens[ix].formula == "Falso") {
             if (resulty == false) {
               resulty = true
             } else { resulty = false }
@@ -372,15 +498,26 @@ table{width:100%}
             const start3 = this.evalMessage(data.itens[ix].start3, cache).replace("\\n", "\n");
             const end3 = this.evalMessage(data.itens[ix].end3, cache).replace("\\n", "\n");
             resultitem = start3 + resultitem + end3;
+            resultitem2 = start3 + resultitem2 + end3;
           }
 
         }
 
       }
 
+      if (data.checkend == true) {
 
+        if (data.endlib == true) {
+          result += resultitem2 + eval(String(this.evalMessage(data.endeval, cache)))
+        } else {
+          result += resultitem + eval(String(this.evalMessage(data.endeval, cache)))
+        }
 
-      result += resultitem + end
+      } else {
+
+        result += resultitem + end
+
+      }
 
     }
 
