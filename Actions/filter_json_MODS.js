4e5cc3b224c99eabc1d037251cf511c0cef2d8d0
LIK (DANIL) <61782941+likDanil@users.noreply.github.com>
2023-06-11 22:46:17 +0500

    Новое действие! (Версия 0.1)

---
+++ Actions/filter_json_MODS.js
@@ -0,0 +1,342 @@
+module.exports = {
+  name: 'Filter JSON MOD',
+  section: 'Lists and Loops',
+  meta: {
+    version: '2.1.7',
+    preciseCheck: true,
+    author: '[xinxyla - 172782058396057602]',
+    authorUrl: 'https://github.com/DBM-Mods/Russia',
+    downloadURL: 'https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip',
+  },
+
+  subtitle: function (data, presets) {
+    const storage = presets.variables;
+
+    if (data.descriptionx == true) {
+        desccor = data.descriptioncolor
+    } else {
+        desccor = 'none'
+    }
+
+    const opcao = [
+      "Существует",
+      "Равный",
+      "Точно так же",
+      "Менее, чем",
+      "Меньше или равно",
+      "Больше, чем",
+      "Соответствует регексу",
+      "Длина больше, чем",
+      "Длина меньше",
+      "Длина равна",
+      "Начинается с",
+      "Заканчивается на",
+      "Больше или равно",
+      "Меньше или равно",
+      "Совпадает с полным регексом",
+      `Entre ${data.value} e ${data.value2}`,
+      "Есть ли в нем ударения?",
+      `Оно включает в себя слова ["a", "b", "c"]`,
+      `Оно равно словам ["a", "b", "c"]`,
+      "Это четное число?",
+      "Это нечетное число?",
+      "Это число?",
+      "Это текст?",
+      "Is it a list?", 
+      "Is it a list?",
+      "Is it an image URL?",
+      "Is it a URL?",
+      "Is it a URL?",
+      "That includes ~ Ignore Small Caps/Case",
+      "Это включает ~ Игнорировать ударения", "Это включает ~ Игнорировать ударения",
+      "Включает ~ Игнорировать ударения и малый регистр и заглавную букву",
+    ]
+
+    return data.description
+        ? `<font style="color:${desccor}">${data.description}</font>`
+        : `<font style="color:${desccor}">${opcao[data.type]} ~ ${storage[parseInt(data.storage2, 10)]} (${data.varName2})</font>`
+},
+
+
+
+  variableStorage(data, varType) {
+    if (parseInt(data.storage2, 10) !== varType) return;
+    return ([data.varName2, "Lista"]);
+  },
+
+  fields: ['storage', 'varName', 'type', 'value', 'value2', 'coluna', 'storage2', 'varName2','descriptioncolor','description','descriptionx'],
+
+  html(_isEvent, data) {
+    return `
+    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip">Обновление</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Russia">Версия 0.1</div>
+
+    <div style="width: 100%; padding:5px 5px;height: calc(100vh - 160px);overflow:auto">
+
+    <div id="flutuador" style="padding:0px 0px 15px 0px">
+<table style="width:100%;"><tr>
+<td><span class="dbminputlabel">Описание действия</span><br><input type="text" class="round" id="description" placeholder="Оставьте пустым, чтобы не использовалось!"></td>
+<td style="padding:0px 0px 0px 10px;width:70px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Цвет (вкл)"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
+</tr></table>
+</div>
+  
+<div style="overflow:hidden">
+  <retrieve-from-variable allowSlashParams dropdownLabel="Переменная" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></retrieve-from-variable>
+</div>
+
+
+  <span class="dbminputlabel">Фильтр на основе столбцов (Используйте . для подстолбцов)</span><br>
+  <input type="text" class="round" id="coluna">
+
+  <xinspace>
+
+  <div style="width: 100%">
+    <span class="dbminputlabel">Фильтр</span><br>
+    <select id="type" class="round" onchange="glob.change(this)">
+      <option value="0">Существует</option>
+      <option value="1" selected>Равный</option>
+      <option value="2">Точно так же</option>
+      <option value="3">Менее, чем</option>
+      <option value="13">Меньше или равно</option>
+      <option value="4">Больше, чем</option>
+      <option value="12">Больше или равно</option>
+      <option value="5">Включает</option>
+      <option value="26">Включает ~ Игнорировать Нижний/Верхний Регистр</option>
+      <option value="27">Включает ~ игнорировать акцентуации</option>
+      <option value="28">Включает в себя ~ игнорировать строчные и заглавные & акцентуации</option>
+      <option value="6">Соответствует регулярному выражению</option>
+      <option value="14">Соответствует полному регулярному выражению</option>
+      <option value="7">Длина больше, чем</option>
+      <option value="8">Длина меньше, чем</option>
+      <option value="9">Длина равна</option>
+      <option value="10">Начинается с</option>
+      <option value="11">Заканчивается</option>
+      <option value="15">Между</option>
+      <option value="16">Есть ли в нём акценты?</option>
+      <option value="17">Включает в себя слова  ["a" , "b" , "c"]</option>
+      <option value="18">Это просто как слова  ["a" , "b" , "c"]</option>
+      <option value="19">Это четное число?</option>
+      <option value="20">Это нечетное число?</option>
+      <option value="21">Это число?</option>
+      <option value="24">Это текст?</option>
+      <option value="22">Это список?</option>
+      <option value="23">Это URL-адрес изображения?</option>
+      <option value="25">Это URL?</option>
+    </select>
+  </div>
+
+  <xinspace>
+
+  <div id="xinoculta">
+  <div id="valueDiv" style="float: left; width: 100%">
+    <span class="dbminputlabel">Значение для фильтрации</span><br>
+    <input id="value" class="round" type="text">
+  </div>
+
+  <div id="valueDiv2" style="float: left; width: 35%; padding-left: 18px;">
+    <span class="dbminputlabel">И</span><br>
+    <input id="value2" class="round" type="text">
+  </div>
+
+  <br><br><br></div>
+
+  <div style="float: left; width: 35%; padding-top: 8px;">
+    <span class="dbminputlabel">Хранить в</span><br>
+    <select id="storage2" class="round">
+      ${data.variables[1]}
+    </select>
+  </div>
+
+  <div id="varNameContainer2" style="float: right; width: 60%; padding-top: 8px;">
+    <span class="dbminputlabel">Имя переменной</span><br>
+    <input id="varName2" class="round" type="text">
+  </div>
+</div>
+
+</div>
+
+<style>
+xinspace{padding:16px 0px 0px 0px;display:block}
+.dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
+.dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
+
+</style>`;
+  },
+
+  init() {
+    glob.change = function (event) {
+      if (event.value == "0" || event.value == "16" || event.value == "19" || event.value == "20" || event.value == "21" || event.value == "24" || event.value == "22" || event.value == "23" || event.value == "25") {
+        document.getElementById("valueDiv").style.display = "none";
+        document.getElementById("valueDiv").style.width = "100%";
+        document.getElementById("valueDiv2").style.display = "none";
+        document.getElementById("xinoculta").style.display = "none";
+      } else if (event.value == "15") {
+        document.getElementById("valueDiv").style.width = "65%";
+        document.getElementById("valueDiv").style.display = "block";
+        document.getElementById("valueDiv2").style.display = "block";
+        document.getElementById("xinoculta").style.display = "block";
+      } else {
+        document.getElementById("valueDiv").style.display = "block";
+        document.getElementById("valueDiv").style.width = "100%";
+        document.getElementById("valueDiv2").style.display = "none";
+        document.getElementById("xinoculta").style.display = "block";
+      }
+    };
+
+    glob.change(document.getElementById("type"));
+
+    const xinelaslinks = document.getElementsByClassName('xinelaslink');
+    for (let x = 0; x < xinelaslinks.length; x++) {
+      const xinelaslink = xinelaslinks[x];
+      const url = xinelaslink.getAttribute('data-url');
+      if (url) {
+       xinelaslink.setAttribute('title', url);
+       xinelaslink.addEventListener('click', (e) => {
+          e.stopImmediatePropagation();
+          console.log(`Запуск URL: [${url}] в браузере по умолчанию.`);
+          require('child_process').execSync(`start ${url}`);
+        });
+      }
+    }
+
+  },
+
+  async action(cache) {
+    const data = cache.actions[cache.index];
+    const storage = parseInt(data.storage, 10);
+    const varName = this.evalMessage(data.varName, cache);
+    const variable = this.getVariable(storage, varName, cache);
+    const value = this.evalMessage(data.value, cache);
+    const coluna = this.evalMessage(data.coluna, cache);
+
+    function getNestedValue(obj, columns) {
+      const columnArr = columns.split('.');
+      let colunas = obj;
+      
+      for (let i = 0; i < columnArr.length; i++) {
+        if (colunas && colunas.hasOwnProperty(columnArr[i])) {
+          colunas = colunas[columnArr[i]];
+        } else {
+          return undefined;
+        }
+      }
+      
+      return colunas;
+    }
+
+    const colunasAninhadas = coluna;
+
+    let result
+
+    switch (parseInt(data.type, 10)) {
+      case 0:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas) !== value);
+        break;
+      case 1:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas) == value);
+        break;
+      case 2:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas) === value);
+        break;
+      case 3:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas) < value);
+        break;
+      case 4:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas) > value);
+        break;
+      case 5:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas).toString().includes(value));
+        break;
+      case 6:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas).toString().match(new RegExp('^' + value + '$', 'i')));
+        break;
+      case 7:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas).length > value);
+        break;
+      case 8:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas).length < value);
+        break;
+      case 9:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas).length == value);
+        break;
+      case 10:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas).toString().startsWith(value));
+        break;
+      case 11:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas).toString().endsWith(value));
+        break;
+      case 12:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas) >= value);
+        break;
+      case 13:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas) <= value);
+        break;
+      case 14:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas).toString().match(new RegExp(value)));
+        break;
+      case 15:
+        const value2 = this.evalMessage(data.value2, cache);
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas) >= value && item <= value2);
+        break;
+      case 16:
+        const conditions = ["Ä", "Å", "Á", "Â", "À", "Ã", "Ā", "Ă", "Ą", "ā", "ă", "ą", "ä", "á", "â", "à", "ã", "É", "Ê", "Ë", "È", "Ė", "Ę", "Ě", "Ĕ", "Ē", "ė", "ę", "ě", "ĕ", "ē", "é", "ê", "ë", "è", "Í", "Î", "Ï", "Ì", "İ", "Į", "Ī", "ı", "į", "ī", "í", "î", "ï", "ì", "Ö", "Ó", "Ô", "Ò", "Õ", "Ő", "Ō", "ő", "ō", "ö", "ó", "ô", "ò", "õ", "Ü", "Ú", "Û", "Ų", "Ű", "Ů", "Ū", "ų", "ű", "ů", "ū", "ü", "ú", "û", "ù", "Ç", "Ć", "Č", "ç", "ć", "č", "Ñ", "Ň", "Ņ", "Ń", "ñ", "ň", "ņ", "ń", "Ÿ", "Ý", "ý", "Ź", "Ż", "Ž", "ź", "ż", "ž", "Ł", "Ľ", "Ļ", "Ĺ", "ł", "ľ", "ĺ", "Ķ", "ķ", "Ģ", "Ğ", "ģ", "ğ", "Ď", "ď", "Ś", "Š", "Ş", "ś", "š", "ş", "Ť", "Ț", "Ţ", "ť", "ț", "ţ", "Ŕ", "Ř", "ŕ", "ř"]
+        result = variable.filter((item) => conditions.some(el => getNestedValue(item, colunasAninhadas).toString().includes(el)));
+        break;
+      case 17:
+        const conditionsX = [...value];
+        result = variable.filter((item) => conditionsX.some(els => getNestedValue(item, colunasAninhadas).toString().includes(els)));
+        break;
+      case 18:
+        const conditionsZ = [...value];
+        result = variable.filter((item) => conditionsZ.some(elz => getNestedValue(item, colunasAninhadas) == (elz)));
+        break;
+      case 19:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas) % 2 == 0);
+        break;
+      case 20:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas) % 2 == 1);
+        break;
+      case 21:
+        result = variable.filter((item) => isNaN(parseFloat(getNestedValue(item, colunasAninhadas).toString().replace(",", "."))) == false);
+        break;
+      case 22:
+        result = variable.filter((item) => Array.isArray(getNestedValue(item, colunasAninhadas)) == true);
+        break;
+      case 23:
+        const isImageUrl = require('is-image-url');
+        result = variable.filter((item) => isImageUrl(getNestedValue(item, colunasAninhadas).toString()) == true);
+        break;
+      case 24:
+        result = variable.filter((item) => Boolean(typeof getNestedValue(item, colunasAninhadas) === "string") == true);
+        break;
+      case 25:
+        const isUrl = require("is-url");
+        result = variable.filter((item) => isUrl(getNestedValue(item, colunasAninhadas).toString()) == true);
+        break;
+      case 26:
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas).toLowerCase().includes(value.toLowerCase()))
+        break;
+      case 27:
+        tratar = value.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas).normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(tratar));
+        break;
+      case 28:
+        tratar = value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+        result = variable.filter((item) => getNestedValue(item, colunasAninhadas).toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(tratar));
+        break;
+    }
+
+    if (result == undefined) {
+      result = []
+    }
+
+
+    const storage2 = parseInt(data.storage2, 10);
+    const varName2 = this.evalMessage(data.varName2, cache);
+    this.storeValue(result, storage2, varName2, cache);
+
+    this.callNextAction(cache);
+  },
+
+  mod() { },
+};
