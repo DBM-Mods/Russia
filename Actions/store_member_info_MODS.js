c8c9c9e1d4f05513ecacafa331866e3269470b8e
MrJackcall <87709785+MrJackcall@users.noreply.github.com>
2023-07-31 22:02:35 +0300

    [FIX] полностью исправлен и переписан перевод
    Предложения по исправлению перевода можете писать в ЛС Discord: @mrjackcall

--- Actions/store_member_info_MODS.js
+++ Actions/store_member_info_MODS.js
@@ -13,47 +13,45 @@ module.exports = {
     const info = [
       "Объект участника",
       "ID участника",
-      "Имя пользователя участника", "Отображаемое имя участника",
-      "Отображаемое имя участника", "Отображаемое имя участника",
-      "Цвет участника", "Цвет участника",
-      "Имя сервера пользователя",
-      "Последнее сообщение пользователя (удалено)",
-      "Самая высокая позиция участника", "Самая высокая позиция участника", "Самая высокая позиция участника", "Самая высокая позиция участника",
-      "Повышенная позиция участника", "Повышенная позиция участника",
-      "Цветовое положение участника", "Цветовое положение участника",
-      "Владеет ли участник?",
-      "Является ли участник немым?",
-      "Является ли участник глухим?", 
-      "Является ли участник глухим?",
-      "Может ли участник быть изгнанным?",
-      "Игровой статус участника",
-      "Статус пользователя",
-      "URL аватара пользователя",
-      "Список рангов пользователя",
-      "Количество сообщений пользователя",
-      "Голосовой канал пользователя",
+      "Юзернейм участника",
+      "Отображаемое на сервере имя участника",
+      "Цвет участника",
+      "Имя сервера участника",
+      "Последнее сообщение участника (удалено)",
+      "Наивысшая роль участника",
+      "Повышающая участника роль (отображаемая справа)",
+      "Наивысшая цветная роль участника",
+      "Является ли участник владельцем?",
+      "Выключен ли микрофон у участника?",
+      "Выключен ли звук у частника",
+      "Может ли участник быть забаненным?",
+      "Имя игрового статуса участника",
+      "Пользовательский статус участника",
+      "URL аватара участника",
+      "Список ролей участника",
+      "Количество ролей участника",
+      "Голосовой канал участника",
       "Дискриминатор участника",
-      "Метка участника",
-      "Учетная запись участника, созданная в",
-      "Временная метка аккаунта, созданного пользователем",
-      "Member logged in on", "Участник вошел в систему",
-      "Временная метка входа участника на сервер",
+      "Тег участника",
+      "Учетная запись участника создана",
+      "Метка времени создания аккаунта участника",
+      "Дата входа участника на сервер",
+      "Метка времени входа участника на сервер",
       "ID последнего сообщения (удалено)",
       "Список разрешений участника",
       "Список значков участника",
-      "Клиентский статус участника",
+      "Статус клиента участника",
       "Пользовательский статус участника",
-      "URL-адрес аватара сервера участника",
-      "Member expired on",
-      "Временная метка участника истекла на",
+      "URL отображаемого аватара участника",
+      "Время истечения Тайм-аута участника",
+      "Метка времени истечения Тайм-аута участника",
       "URL баннера участника",
       "ID сервера участника",
-      "Временная метка импульса участника",
-      "Общее количество приглашений участников на текущем сервере",
-      "Список приглашений участников на текущем сервере",
-      "Список использования приглашений участников на текущем сервере", 
-      "Список использования приглашений участников на текущем сервере",
-      "Общее количество использований приглашений участников на текущем сервере",
+      "Метка времени Буста участником",
+      "Всего приглашений участника на текущем сервере",
+      "Список приглашений участника на текущий сервер",
+      "Список использований приглашений участника на текущий сервер",
+      "Всего использований приглашений участника на текущий сервер",
     ];
 
     if (data.descriptionx) {
@@ -72,130 +70,130 @@ module.exports = {
     const type = parseInt(data.storage, 10);
     if (type !== varType) return;
     const info = parseInt(data.info, 10);
-    let dataType = "Unknown Type";
+    let dataType = "Неизвестный тип";
 
     switch (info) {
       case 0:
-        dataType = "Пользователь сервера";
+        dataType = "Объект участника";
         break;
-    case 1:
+      case 1:
         dataType = "ID";
         break;
-    case 2:
-        dataType = "Текст";
-        break
-    case 3:
-        dataType = "Текст";
-        break
-    case 4:
+      case 2:
         dataType = "Текст";
         break;
-    case 5:
+      case 3:
         dataType = "Текст";
         break;
-    case 6:
+      case 4:
         dataType = "Текст";
         break;
-    case 7:
-        dataType = "Позиция";
-        break;
-    case 8:
-        dataType = "Позиция";
-        break;
-    case 9:
-        dataType = "Позиция";
-        break;
-    case 10:
-        dataType = "Да/Нет";
-        break;
-    case 11:
-        dataType = "Да/Нет";
-        break;
-    case 12:
-        dataType = "Да/Нет";
-        break;
-    case 13:
-        dataType = "Да/Нет";
-        break;
-    case 14:
+      case 5:
         dataType = "Текст";
         break;
-    case 15:
+      case 6:
         dataType = "Текст";
         break;
-    case 16:
-        dataType = "URL";
+      case 7:
+        dataType = "Роль";
         break;
-    case 17:
+      case 8:
+        dataType = "Роль";
+        break;
+      case 9:
+        dataType = "Роль";
+        break;
+      case 10:
+        dataType = "True/False";
+        break;
+      case 11:
+        dataType = "True/False";
+        break;
+      case 12:
+        dataType = "True/False";
+        break;
+      case 13:
+        dataType = "True/False";
+        break;
+      case 14:
+        dataType = "Текст";
+        break;
+      case 15:
+        dataType = "Текст";
+        break;
+      case 16:
+        dataType = "URL"
+        break;
+      case 17:
         dataType = "Список";
         break;
-    case 18:
+      case 18:
         dataType = "Число";
         break;
-    case 19:
+      case 19:
         dataType = "Канал";
         break;
-    case 20:
+      case 20:
         dataType = "Число";
         break;
-    case 21:
+      case 21:
         dataType = "Текст";
         break;
-    case 22:
+      case 22:
         dataType = "Дата";
         break;
-    case 23:
-        dataType = "Временная метка";
+      case 23:
+        dataType = "Timestamp";
         break;
-    case 24:
-        dataType = "Дата";
+      case 24:
+        dataType = "Data";
         break;
-    case 25:
-        dataType = "Временная метка";
+      case 25:
+        dataType = "Timestamp";
         break;
-    case 26:
+      case 26:
         dataType = "ID";
         break;
-    case 27:
+      case 27:
         dataType = "Список";
         break;
-    case 28:
+      case 28:
         dataType = "Список";
         break;
-    case 29:
+      case 29:
         dataType = "Текст";
         break;
-    case 30:
+      case 30:
         dataType = "Текст";
         break;
-    case 31:
+      case 31:
         dataType = "URL";
         break;
-    case 32:
+      case 32:
         dataType = "Дата";
         break;
-    case 33:
-        dataType = "Временная метка";
+      case 33:
+        dataType = "Timestamp";
         break;
-    case 34:
+      case 34:
         dataType = "URL";
         break;
-    case 35:
+      case 35:
         dataType = "ID";
         break;
-    case 36:
-        dataType = "Временная метка";
+      case 36:
+        dataType = "Timestamp";
         break;
-    case 37:
+      case 37:
         dataType = "Число";
         break;
-    case 38:
+      case 38:
         dataType = "Список";
         break;
-    case 39:
+      case 39:
         dataType = "Список";
         break;
-    case 40:
+      case 40:
         dataType = "Число";
         break;
     }
@@ -207,7 +205,7 @@ module.exports = {
 
   html(isEvent, data) {
     return `
-    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zipм">Обновление</div>
+    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip">Версия</div>
     <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Russia">Версия 1.4</div>
 
     <div style="width: 100%; padding:5px 0px;height: calc(100vh - 160px);overflow:auto">
@@ -217,7 +215,7 @@ module.exports = {
         <td>
         <span class="dbminputlabel">Описание действия</span>
         <br>
-        <input type="text" class="round" id="description" placeholder="Оставьте пустым, чтобы не использовалось!">
+        <input type="text" class="round" id="description" placeholder="Не обязательное поле">
         </td>
         <td style="padding:0px 0px 0px 10px;width:70px">
         <div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px">
@@ -235,56 +233,56 @@ module.exports = {
 
 
 <div style="padding-top: 8px;">
-	<span class="dbminputlabel">Информация</span><br>
-	<select id="info" class="round2">
-  <option value="0" selected>Объект участника</option>
-  <option value="1">Идентификатор участника</option>
-  <option value="2">Имя пользователя участника</option>
-  <option value="3">Образное имя участника [Nickname]</option>
-  <option value="21">Тег участника</option>
-  <option value="20">Дискриминатор участника</option>
-  <option value="4">Цвет участника</option>
-  <option value="15">Статус участника</option>
-  <option value="16">URL аватара участника</option>
-  <option value="34">URL баннера участника</option>
-  <option value="31">URL аватара сервера пользователя</option>
-  <option value="5">Имя сервера участника</option>
-  <option value="35">Идентификатор сервера участника</option>
-  <option value="6">Последнее сообщение пользователя (удалено)</option>
-  <option value="26">Идентификатор последнего сообщения пользователя (удален)</option>
-  <option value="7">Высшее звание участника</option>
-  <option value="8">Высшее звание участника</option>
-  <option value="9">Цветовая позиция участника</option>
-  <option value="17">Список рангов участника</option>
-  <option value="18">Количество позиций участника</option>
-  <option value="10">Является ли пользователь владельцем?
-  <option value="11">Выключен ли звук у пользователя?
-  <option value="12">Глухой ли участник? </option>
-  <option value="13">Можно ли запретить пользователя? </option>
-  <option value="14">Наименование игрового статуса участника</option>
-  <option value="30">Название пользовательского статуса участника</option>
-  <option value="19">Голосовой канал пользователя</option>
-  <option value="22">Учетная запись участника создана на</option>
-  <option value="23">Временная метка учетной записи, созданной участником</option>
-  <option value="24">участник присоединился к серверу</option>
-  <option value="25">Временная метка пользователя, который присоединился к серверу</option>
-  <option value="27">Список разрешений участника</option>
-  <option value="28">Список бейджей участника</option>
-  <option value="29">Статус клиента участника [веб или мобильный]</option>
-  <option value="32">Время ожидания участника</option>
-  <option value="33">Временная метка наказанного участника</option>
-  <option value="36">Временная метка импульса участника</option>
-  <option value="37">Общее количество приглашений участника на текущем сервере</option>
-  <option value="38">Список приглашений участника на текущем сервере</option>
-  <option value="39">Список использований приглашений участника на текущем сервере</option>
-  <option value="40">Общее количество использований приглашений участника на текущем сервере</option>
-	</select>
-  <input type="text" id="filtrodoxinxyla" class="round" placeholder="Параметры фильтра...">
-  </div>
+  <span class="dbminputlabel">Информация</span><br>
+  <select id="info" class="round2">
+    <option value="0" selecionado>Объект участника</option>
+    <option value="1">ID участника</option>
+    <option value="2">Юзернейм участника</option>
+    <option value="3">Отображаемое на сервере имя участника [Псевдоним]</option>
+    <option value="21">Тег участника</option>
+    <option value="20">Дискриминатор участника (#0000)</option>
+    <option value="4">Цвет участника</option>
+    <option value="15">Пользовательский статус участника</option>
+    <option value="16">URL: аватар участника</option>
+    <option value="34">URL: баннер участника</option>
+    <option value="31">URL: отображаемый на сервере аватар участника</option>
+    <option value="5">Имя сервера участника</option>
+    <option value="35">ID сервера участника</option>
+    <option value="6">Последнее сообщение участника (удалено)</option>
+    <option value="26">ID последнего сообщения участника (удалено)</option>
+    <option value="7">Наивысшая роль участника</option>
+    <option value="8">Повышающая участника роль (отображаемая справа)</option>
+    <option value="9">Наивысшая цветная роль участника</option>
+    <option value="17">Список ролей участника</option>
+    <option value="18">Количество ролей участника</option>
+    <option value="10">Является ли участник владельцем? (true | false)</option>
+    <option value="11">Выключен ли микрофон у участника? (true | false)</option>
+    <option value="12">Выключен ли звук у участника? (true | false)</option>
+    <option value="13">Может ли участник быть забаненным? (true | false)</option>
+    <option value="14">Имя игрового статуса участника</option>
+    <option value="30">Пользовательский статус участника</option>
+    <option value="19">Голосовой канал участника</option>
+    <option value="22">Учетная запись участника создана</option>
+    <option value="23">Метка времени создания учетной записи участника</option>
+    <option value="24">Участник присоединился к серверу</option>
+    <option value="25">Метка времени присоединения участника к серверу</option>
+    <option value="27">Список разрешений участника</option>
+    <option value="28">Список значков участника</option>
+    <option value="29">Статус клиента участника [Веб или Мобильное]</option>
+    <option value="32">Время истечения Тайм-аута участника</option>
+    <option value="33">Метка времени истечения Тайм-аута участника</option>
+    <option value="36">Метка времени Буста участником (?)</option>
+    <option value="37">Общее количество приглашений участника на текущем сервере</option>
+    <option value="38">Список приглашений участника на текущем сервере</option>
+    <option value="39">Список использований приглашений участника на текущем сервере</option>
+    <option value="40">Общее количество использований приглашений участника на текущем сервере</option>
+  </select>
+  <input type="text" id="filtrodoxinxyla" class="round" placeholder="Фильтровать опции...">
+</div>
 
 <br>
 
-<store-in-variable dropdownLabel="Сохранить в" selectId="storage" variableContainerId="varNameContainer2" variableInputId="varName2"></store-in-variable>
+<store-in-variable dropdownLabel="Хранить в" selectId="storage" variableContainerId="varNameContainer2" variableInputId="varName2"></store-in-variable>
 
 </div>
 
@@ -332,6 +330,7 @@ module.exports = {
   border: 1px solid #777;
   background: rgba(255,255,255,0.1);
 }
+
 .dbmmodsbr1 {
   position: absolute;
   bottom: 0px;
@@ -368,7 +367,7 @@ module.exports = {
         xinelaslink.setAttribute('title', url);
         xinelaslink.addEventListener('click', (e) => {
           e.stopImmediatePropagation();
-          console.log(`Запуск URL: [${url}] в браузере по умолчанию.`);
+          console.log(`Запуск URL: [${url}] в стандартном браузере.`);
           require('child_process').execSync(`start ${url}`);
         });
       }
@@ -487,10 +486,10 @@ module.exports = {
         if (member.presence?.status) {
           const status = member.presence.status;
           switch (status) {
-            case "online": { result = "В сети"; break; }
-            case "offline": { result = "Невидимый"; break; }
-            case "idle": { result = "Неактивен"; break; }
-            case "dnd": { result = "Не беспокоить"; break; }
+            case "online": { result = "Online"; break; }
+            case "offline": { result = "Offline"; break; }
+            case "idle": { result = "Ausente"; break; }
+            case "dnd": { result = "Ocupado"; break; }
           }
         }
         break;
