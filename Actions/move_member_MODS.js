1ce700588ffba659f16196e34fb1966995225ade
MrJackcall <z-kirill04@mail.ru>
2023-08-03 22:43:22 +0300

    Новое действие [v 0.2]
    Предложения по исправлению перевода можете писать в ЛС Discord: @mrjackcall

---
+++ Actions/move_member_MODS.js
@@ -0,0 +1,54 @@
+module.exports = {
+
+  name: "Move Member MOD",
+  section: "Other Stuff",
+  meta: {
+    version: '2.1.7',
+    preciseCheck: true,
+    author: '[XinXyla - 172782058396057602]',
+    authorUrl: 'https://github.com/DBM-Mods/Russia',
+    downloadURL: 'https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip',
+  },
+
+  subtitle(data, presets) {
+    return `${presets.getMemberText(data.member, data.varName)}`;
+  },
+
+  fields: ["member", "varName", "channel", "varName2",],
+
+  html(isEvent, data) {
+    return `
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Версия 0.2</div>
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+    <member-input dropdownLabel="Участник" selectId="member" variableContainerId="varNameContainer" variableInputId="varName"></member-input>
+
+    <br><br><br><br>
+
+    <voice-channel-input dropdownLabel="Голосовой Канал" selectId="channel" variableContainerId="varNameContainer2" variableInputId="varName2" selectWidth="45%" variableInputWidth="50%"></voice-channel-input>
+    `;
+  },
+
+  init() {},
+
+  async action(cache) {
+    const data = cache.actions[cache.index];
+    const member = await this.getMemberFromData(data.member, data.varName, cache);
+    if (!member) {
+      this.callNextAction(cache);
+      return;
+    }
+    const targetChannel = await this.getVoiceChannelFromData(data.channel, data.varName2, cache);
+    if (!targetChannel) {
+      this.callNextAction(cache);
+      return;
+    }
+
+    member.voice.setChannel(targetChannel.id).catch((err) => {
+      console.log('ОШИБКА')
+      console.error(err)
+    });
+    this.callNextAction(cache);
+  },
+
+  mod() {},
+};
