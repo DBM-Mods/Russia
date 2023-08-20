9e0ac3407c0662e4305e572061d7da56758da6d7
MrJackcall <z-kirill04@mail.ru>
2023-08-20 13:49:46 +0300

    Обновления действия [v0.2]
    Предложения по исправлению перевода можете писать в ЛС Discord: @mrjackcall

--- Actions/store_channel_info_MODS.js
+++ Actions/store_channel_info_MODS.js
@@ -4,25 +4,25 @@ module.exports = {
   meta: {
     version: '2.1.7',
     preciseCheck: true,
-    author: '[XinXyla - 172782058396057602]',
+    author: '[xinxyla - 172782058396057602]',
     authorUrl: 'https://github.com/DBM-Mods/Russia',
     downloadURL: 'https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip',
-    },
+  },
 
   subtitle(data, presets) {
     const info = [
       "Объект канала",
       "ID канала",
-      "Название канала",
+      "Имя канала",
       "Тема канала",
-      "Последнее сообщение канала",
+      "Последнее сообщение в канале",
       "Позиция канала",
-      "Канал NSFW?",
-      "Это прямое сообщение?",
-      "Можно ли удалить канал?",
+      "Это NSFW-канал?",
+      "Это личное сообщение?",
+      "Канал может быть удален?",
       "Дата создания канала",
       "ID категории канала",
-      "Название категории канала",
+      "Имя категории канала",
       "Timestamp создания канала",
       "Это текстовый канал?",
       "Это голосовой канал?",
@@ -30,23 +30,33 @@ module.exports = {
       "Это канал новостей?",
       "Это трибуна?",
       "Тип канала",
-      "Права участника в канале [Текстовый режим]",
-      "Права канала [Текстовый режим]",
-      "Права участника в канале [Режим Объекта]",
-      "Права канала [Режим Объекта]",
-      "Права участника в канале [Режим Списка]",
-      "Права канала [Режим Списка]",
-      "Медленный режим канала [секунды]",
-      "Тема канала [режим списка с объектом]",
+      "Права пользователей на канале [режим текста]",
+      "Права ролей на канале [режим текста]",
+      "Права пользователей на канале [режим объекта]",
+      "Права ролей на канале [режим объекта]",
+      "Права пользователей на канале [режим списка]",
+      "Права ролей на канале [режим списка]",
+      "Режим медленного режима канала [секунды]",
+      "Темы каналов [режим списка с объектом]",
+      "Список сообщений канала",
     ];
-    return `${presets.getChannelText(data.channel, data.varName)} - ${info[parseInt(data.info, 10)]}`;
+
+    if (data.descriptionx) {
+      desccor = data.descriptioncolor;
+    } else {
+      desccor = "none";
+    }
+
+    return data.description
+      ? `<font style="color:${desccor}">${data.description}</font>`
+      : `<font style="color:${desccor}">${presets.getChannelText(data.channel, data.varName)} - ${info[parseInt(data.info, 10)]}</font>`
   },
 
   variableStorage(data, varType) {
     const type = parseInt(data.storage, 10);
     if (type !== varType) return;
     const info = parseInt(data.info, 10);
-    let dataType = "Unknown Type";
+    let dataType = "Неизвестный Тип";
     switch (info) {
       case 0:
         dataType = "Объект";
@@ -60,24 +70,24 @@ module.exports = {
       case 3:
         dataType = "Текст";
         break;
-        case 4:
-          dataType = "Текст";
-          break;
+      case 4:
+        dataType = "Текст";
+        break;
       case 5:
         dataType = "Число";
         break;
-        case 6:
-          dataType = "true/false";
-          break;
-          case 7:
-            dataType = "true/false";
-            break;
+      case 6:
+        dataType = "true/false";
+        break;
+      case 7:
+        dataType = "true/false";
+        break;
       case 8:
         dataType = "true/false";
         break;
-        case 9:
-          dataType = "Data";
-          break;
+      case 9:
+        dataType = "Дата";
+        break;
       case 10:
         dataType = "Число";
         break;
@@ -87,62 +97,84 @@ module.exports = {
       case 12:
         dataType = "Timestamp";
         break;
-        case 13:
-          dataType = "true/false";
-          break;
-          case 14:
-            dataType = "true/false";
-            break;
-            case 15:
-              dataType = "true/false";
-              break;
-              case 16:
-                dataType = "true/false";
-                break;
-                case 17:
-                  dataType = "true/false";
-                  break;
-                  case 18:
-                    dataType = "Текст";
-                    break;
-                    case 19:
-                      dataType = "Текст";
-                      break;
-                      case 20:
-                        dataType = "Текст";
-                        break;
-                        case 21:
-                          dataType = "Объект";
-                          break;
-                          case 22:
-                            dataType = "Объект";
-                            break;
-                            case 23:
-                              dataType = "Список";
-                              break;
-                              case 24:
-                                dataType = "Список";
-                                break;
-                                case 25:
-                                  dataType = "Число";
-                                  break;
-                                  case 26:
-                                    dataType = "Список/Объект";
-                                    break;
+      case 13:
+        dataType = "true/false";
+        break;
+      case 14:
+        dataType = "true/false";
+        break;
+      case 15:
+        dataType = "true/false";
+        break;
+      case 16:
+        dataType = "true/false";
+        break;
+      case 17:
+        dataType = "true/false";
+        break;
+      case 18:
+        dataType = "Текст";
+        break;
+      case 19:
+        dataType = "Текст";
+        break;
+      case 20:
+        dataType = "Текст";
+        break;
+      case 21:
+        dataType = "Объект";
+        break;
+      case 22:
+        dataType = "Объект";
+        break;
+      case 23:
+        dataType = "Список";
+        break;
+      case 24:
+        dataType = "Список";
+        break;
+      case 25:
+        dataType = "Число";
+        break;
+      case 26:
+        dataType = "Список/Объект";
+        break;
+      case 27:
+        dataType = "Список/Объект";
+        break;
     }
     return [data.varName2, dataType];
   },
 
-  fields: ["channel", "varName", "info", "storage", "varName2"],
+  fields: ["channel", "varName", "info", "storage", "varName2", "description", "descriptionx", "descriptioncolor"],
 
   html(isEvent, data) {
     return `
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Версия 0.1</div>
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip">Обновить</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Russia">Версия 0.2</div>
 
+    <div style="width: 100%; padding:5px 0px;height: calc(100vh - 160px);overflow:auto">
+
+  <div id="flutuador" style="padding:0px 0px 15px 0px">
+    <table style="width:100%;"><tr>
+        <td>
+        <span class="dbminputlabel">Описание действия</span>
+        <br>
+        <input type="text" class="round" id="description" placeholder="Не обязательно поле">
+        </td>
+        <td style="padding:0px 0px 0px 10px;width:70px">
+        <div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px">
+            <dbm-checkbox id="descriptionx" label="Цвет (вкл)"></dbm-checkbox>
+        </div>
+        <br>
+        <input type="color" value="#ffffff" class="round" id="descriptioncolor">
+        </td>
+    </table>
+  </div>
+
+  <div style="overflow: hidden">
 <channel-input dropdownLabel="Целевой канал" selectId="channel" variableContainerId="varNameContainer" variableInputId="varName"></channel-input>
-
-<br><br><br>
+</div>
 
 <div style="padding-top: 8px;">
 	<span class="dbminputlabel">Целевая информация</span><br>
@@ -172,7 +204,7 @@ module.exports = {
   <option value="14">Это голосовой канал?</option>
   <option value="15">Это категория?</option>
   <option value="16">Это канал новостей?</option>
-  <option value="17">Это Stage канал?</option>
+  <option value="17">Это трибуна?</option>
   </optgroup>
   <optgroup label="Разрешения канала">
   <option value="19">Права доступа пользователей канала [текстовый режим]</option>
@@ -182,15 +214,166 @@ module.exports = {
   <option value="23">Права доступа пользователей канала [режим списка]</option>
   <option value="24">Права канала [режим списка]</option>
   </optgroup>
+  <optgroup label="Сообщения канала">
+  <option value="27">Список сообщений канала [Объект]</option>
+  </optgroup>
 	</select>
+  <input type="text" id="filtrodoxinxyla" class="round" placeholder="Фильтр элементов...">
 </div>
 
 <br>
 
-<store-in-variable dropdownLabel="Сохранить в" selectId="storage" variableContainerId="varNameContainer2" variableInputId="varName2"></store-in-variable>`;
+<store-in-variable dropdownLabel="Сохранить в" selectId="storage" variableContainerId="varNameContainer2" variableInputId="varName2"></store-in-variable>
+
+<style>
+.dbmmodsbr1 {
+  position: absolute;
+  bottom: 0px;
+  border: 0px solid rgba(50,50,50,0.7);
+  background: rgba(0,0,0,0.7);
+  color: #999;
+  padding: 5px;
+  left: 0px;
+  z-index: 999999;
+  cursor: pointer
+}
+
+.dbmmodsbr2 {
+  position: absolute;
+  bottom: 0px;
+  border: 0px solid rgba(50,50,50,0.7);
+  background: rgba(0,0,0,0.7);
+  color: #999;
+  padding: 5px;
+  right: 0px;
+  z-index: 999999;
+  cursor: pointer
+}
+.round2{width:100%;height:30px;outline:0}
+.round2 option{padding:3px 8px;text-align:left}
+.round2 optgroup{text-align:center;padding:4px 0px;}
+
+
+.abrir {
+  min-height: 30px;
+  height: 30px;
+  animation: abrir .5s forwards;
+}
+
+@keyframes abrir {
+  from {
+    min-height: 30px;
+    height: 30px;
+  }
+  to {
+    min-height: 100px;
+    height: calc(100vh - 420px);
+  }
+}
+
+.fechar {
+  min-height: 100px;
+  height: calc(100vh - 420px);
+  animation: fechar .5s forwards;
+}
+
+@keyframes fechar {
+  from {
+    min-height: 100px;
+    height: calc(100vh - 420px);
+  }
+  to {
+    min-height: 30px;
+    height: 30px;
+  }
+}
+</style>`;
   },
 
-  init() {},
+  init() {
+    const { glob, document } = this
+
+    const xinelaslinks = document.getElementsByClassName("xinelaslink");
+    for (let x = 0; x < xinelaslinks.length; x++) {
+      const xinelaslink = xinelaslinks[x];
+      const url = xinelaslink.getAttribute('data-url');
+      if (url) {
+        xinelaslink.setAttribute('title', url);
+        xinelaslink.addEventListener('click', (e) => {
+          e.stopImmediatePropagation();
+          console.log(`Запуск URL: [${url}] в браузере по умолчанию.`);
+          require('child_process').execSync(`start ${url}`);
+        });
+      }
+    }
+
+    document.getElementById("info").addEventListener("click", function () {
+      document.getElementById("info").classList.add("abrir");
+      document.getElementById("info").classList.remove("fechar");
+      this.size = this.options.length;
+    });
+
+    document.getElementById("info").addEventListener("blur", function () {
+      this.size = 1;
+      document.getElementById("info").classList.remove("abrir");
+      document.getElementById("info").classList.add("fechar");
+      document.getElementById("info").style.height = "30px";
+    });
+
+    document.getElementById("filtrodoxinxyla").addEventListener("keyup", function () {
+      var select = document.getElementById("info");
+      var optgroups = select.getElementsByTagName("optgroup");
+      var filter = this.value.toLowerCase();
+      var options = document.getElementById("info").options;
+      for (var i = 0; i < options.length; i++) {
+        var option = options[i];
+        if (option.text.toLowerCase().indexOf(filter) === -1) {
+          option.style.display = "none";
+        } else {
+          option.style.display = "";
+        }
+      }
+
+      for (var i = 0; i < optgroups.length; i++) {
+        var optgroup = optgroups[i];
+        var options = optgroup.getElementsByTagName("option");
+        var visibleOptions = 0;
+        for (var j = 0; j < options.length; j++) {
+          if (options[j].style.display !== "none") {
+            visibleOptions++;
+          }
+        }
+        if (visibleOptions === 0) {
+          optgroup.style.display = "none";
+        } else {
+          optgroup.style.display = "";
+        }
+      }
+
+      document.getElementById("info").dispatchEvent(new Event("click"));
+    });
+
+    var select = document.getElementById("my-select");
+    var optgroups = select.getElementsByTagName("optgroup");
+
+    for (var i = 0; i < optgroups.length; i++) {
+      var optgroup = optgroups[i];
+      var options = optgroup.getElementsByTagName("option");
+      var visibleOptions = 0;
+      for (var j = 0; j < options.length; j++) {
+        if (options[j].style.display !== "none") {
+          visibleOptions++;
+        }
+      }
+      if (visibleOptions === 0) {
+        optgroup.style.display = "none";
+      } else {
+        optgroup.style.display = "";
+      }
+    }
+
+
+  },
 
   async action(cache) {
     const data = cache.actions[cache.index];
@@ -258,17 +441,17 @@ module.exports = {
       case 16:
         result = targetChannel instanceof DiscordJS.NewsChannel;
         break;
-      case 17: 
+      case 17:
         result = targetChannel instanceof DiscordJS.StoreChannel;
         break
       case 18:
         result = targetChannel.type;
         break;
       case 19:
-        result = await targetChannel.permissionOverwrites.cache.filter(p => p.type=='member').map(p => `**Пользователь**: <@${p.id}>\n • Разрешено: ${p.allow.toArray().length>=1?p.allow.toArray():'Ничего'}\n • Отключено: ${p.deny.toArray().length>=1?p.deny.toArray():'Ничего'}`).join('\n\n').replace(/_/g, ' ').toLowerCase();
+        result = await targetChannel.permissionOverwrites.cache.filter(p => p.type == 'member').map(p => `**Пользователь**: <@${p.id}>\n • Разрешено: ${p.allow.toArray().length >= 1 ? p.allow.toArray() : 'Ничего'}\n • Запрещено: ${p.deny.toArray().length >= 1 ? p.deny.toArray() : 'Ничего'}`).join('\n\n');
         break;
-       case 20:
-        result = await targetChannel.permissionOverwrites.cache.filter(p => p.type=='role').map(p => `**Роль**: <@&${p.id}>\n • Разрешено: ${p.allow.toArray().length>=1?p.allow.toArray():'Ничего'}\n • Запрещено: ${p.deny.toArray().length>=1?p.deny.toArray():'Ничего'}`).join('\n\n').replace(/_/g, ' ').toLowerCase();
+      case 20:
+        result = await targetChannel.permissionOverwrites.cache.filter(p => p.type == 'role').map(p => `**Роль**: <@&${p.id}>\n • Разрешено: ${p.allow.toArray().length >= 1 ? p.allow.toArray() : 'Ничего'}\n • Запрещено: ${p.deny.toArray().length >= 1 ? p.deny.toArray() : 'Ничего'}`).join('\n\n');
         break;
       case 21:
         result = await targetChannel.permissionOverwrites.cache;
@@ -277,21 +460,36 @@ module.exports = {
         result = await targetChannel.permissionOverwrites.cache;
         break;
       case 23:
-        result = await targetChannel.permissionOverwrites.cache.filter(p => p.type=='member').map(p => `**Пользователь**: <@${p.id}>\n • Разрешено: ${p.allow.toArray().length>=1?p.allow.toArray():'Ничего'}\n • Запрещено: ${p.deny.toArray().length>=1?p.deny.toArray():'Ничего'}`);
+        result = await targetChannel.permissionOverwrites.cache.filter(p => p.type == 'member').map(p => `**Пользователь**: <@${p.id}>\n • Разрешено: ${p.allow.toArray().length >= 1 ? p.allow.toArray() : 'Ничего'}\n • Запрещено: ${p.deny.toArray().length >= 1 ? p.deny.toArray() : 'Ничего'}`);
         break;
       case 24:
-       result = await targetChannel.permissionOverwrites.cache.filter(p => p.type=='role').map(p => `**Роль**: <@&${p.id}>\n • Разрешено: ${p.allow.toArray().length>=1?p.allow.toArray():'Ничего'}\n • Запрещено: ${p.deny.toArray().length>=1?p.deny.toArray():'Ничего'}`);
-       break;
-       case 25:
-       result = targetChannel.rateLimitPerUser;
-       break;
-       case 26:
-      result = targetChannel.threads.cache.filter(p => p).map(p => p);
+        result = await targetChannel.permissionOverwrites.cache.filter(p => p.type == 'role').map(p => `**Роль**: <@&${p.id}>\n • Разрешено: ${p.allow.toArray().length >= 1 ? p.allow.toArray() : 'Ничего'}\n • Запрещено: ${p.deny.toArray().length >= 1 ? p.deny.toArray() : 'Ничего'}`);
+        break;
+      case 25:
+        result = targetChannel.rateLimitPerUser;
+        break;
+      case 26:
+        result = targetChannel.threads.cache.filter(p => p).map(p => p);
+        break;
+      case 27:
+        const options = {
+          limit: 100
+        };
+        if (cache.msg) {
+          options.before = cache.msg.id;
+        }
+        await targetChannel.messages
+          .fetch(options)
+          .then((messages) => {
+
+            result = messages
+
+          })
+
         break;
       default:
         break;
     }
-    /* Copyright XinXyla#0001 - Replicating to another GitHub is not allowed */
     if (result !== undefined) {
       const storage = parseInt(data.storage, 10);
       const varName2 = this.evalMessage(data.varName2, cache);
@@ -300,5 +498,5 @@ module.exports = {
     this.callNextAction(cache);
   },
 
-  mod() {},
+  mod() { },
 };
