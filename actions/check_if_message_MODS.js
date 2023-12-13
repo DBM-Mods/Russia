094311ea284c86fe74c68faf425fd87355cfd505
LIK (DANIL) <61782941+likDanil@users.noreply.github.com>
2023-12-13 17:49:35 +0500

    Обновление действия [0.2]

--- actions/check_if_message_MODS.js
+++ actions/check_if_message_MODS.js
@@ -4,22 +4,42 @@ module.exports = {
   meta: {
     version: '2.1.7',
     preciseCheck: true,
-    author: '[XinXyla - 172782058396057602]',
+    author: '[xinxyla - 172782058396057602]',
     authorUrl: 'https://github.com/DBM-Mods/Russia',
     downloadURL: 'https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip',
   },
 
   subtitle(data, presets) {
-    return `${presets.getConditionsText(data)}`;
+
+    
+    if (data.descriptionx == true) {
+      desccor = data.descriptioncolor
+    } else {
+      desccor = 'none'
+    }
+
+    return data.description
+      ? `<font style="color:${desccor}">${data.description}</font>`
+      : `<font style="color:${desccor}">${presets.getConditionsText(data)}</font>`
   },
 
-  fields: ['message', 'varName', 'info', "branch"],
+
+  fields: ['message', 'varName', 'info', 'descriptioncolor', 'description', 'descriptionx', 'branch'],
 
   html(isEvent, data) {
     return `
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Версия 0.1</div>
-		<div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
-      <message-input dropdownLabel="Сообщение" selectId="message" variableContainerId="varNameContainer" variableInputId="varName"></message-input>
+    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip">Обновить</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Russia">Версия 0.2</div>
+
+
+    <table style="width:100%;"><tr>
+    <td><span class="dbminputlabel">Описание действия</span><br><input type="text" class="round" id="description" placeholder="Не обязательное поле"></td>
+    <td style="padding:0px 0px 0px 10px;width:70px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Цвет (вкл)"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
+    </tr></table>
+    <br>
+
+
+<message-input dropdownLabel="Сообщение" selectId="message" variableContainerId="varNameContainer" variableInputId="varName"></message-input>
 
 <br><br><br><br>
 
@@ -28,18 +48,20 @@ module.exports = {
   <div style="width: 100%;">
   <span class="dbminputlabel">Проверить сообщение</span><br>
     <select id="info" class="round">
-      <option value="0">Может быть закреплено?</option>
-      <option value="1">Закреплено?</option>
-      <option value="2">Может быть удалено?</option>
-      <option value="3">Удалено?</option>
-      <option value="4">TTS?</option>
-      <option value="5">Системное сообщение?</option>
-      <option value="6">Содержит упоминание пользователя/бота?</option>
-      <option value="7">Содержит более одного упоминания пользователя/бота?</option>
-      <option value="8">Содержит реакции?</option>
-      <option value="9">Содержит вложение?</option>
-      <option value="10">Содержит более одного вложения?</option>
-      <option value="11">Содержит приглашение Discord?</option>
+    <option value="0">Может быть закреплено?</option>
+    <option value="1">Закреплено?</option>
+    <option value="2">Может быть удалено?</option>
+    <option value="3">Удалено?</option>
+    <option value="4">TTS?</option>
+    <option value="5">Системное сообщение?</option>
+    <option value="6">Содержит упоминание пользователя/бота?</option>
+    <option value="7">Содержит более одного упоминания пользователя/бота?</option>
+    <option value="8">Содержит реакции?</option>
+    <option value="9">Содержит вложение?</option>
+    <option value="10">Содержит более одного вложения?</option>
+    <option value="11">Содержит приглашение Discord?</option>
+    <option value="12">Содержит эмбеды?</option>
+    <option value="13">Содержит 2 эмбеда?</option>
     </select>
   </div>
 
@@ -51,6 +73,14 @@ module.exports = {
 <div>
 <conditional-input id="branch" style="padding-top: 8px;"></conditional-input></div>
 
+<style>
+table{width:100%}
+.col1{width:38%;padding:0px 10px 0px 0px}
+.col2{width:60%}
+.dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
+.dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
+
+</style>
 `;
   },
 
@@ -58,7 +88,23 @@ module.exports = {
     return formatters.compatibility_2_0_0_iftruefalse_to_branch(data);
   },
 
-  init() {},
+  init() { 
+    
+    const xinelaslinks = document.getElementsByClassName('xinelaslink');
+    for (let x = 0; x < xinelaslinks.length; x++) {
+      const xinelaslink = xinelaslinks[x];
+      const url = xinelaslink.getAttribute('data-url');
+      if (url) {
+        xinelaslink.setAttribute('title', url);
+        xinelaslink.addEventListener('click', (e) => {
+          e.stopImmediatePropagation();
+          console.log(`Launching URL: [${url}] in your default browser.`);
+          require('child_process').execSync(`start ${url}`);
+        });
+      }
+    }
+    
+  },
 
   async action(cache) {
     const data = cache.actions[cache.index];
@@ -88,29 +134,37 @@ module.exports = {
         break;
       case 6:
         mentions = msg.mentions.users.size
-        if(mentions == 1){result = true}else{result = false}
+        if (mentions == 1) { result = true } else { result = false }
         console.log(mentions)
         break;
-        case 7:
-          mentions = msg.mentions.users.size
-          if(mentions > 1){result = true}else{result = false}
-          break;
-          case 8:
-           react = msg.reactions.cache.size
-           if(react > 0){result = true}else{result = false}
-            break;
-            case 9:
-              att = msg.attachments.size
-              if(att == 1){result = true}else{result = false}
-               break;
-               case 10:
-                att = msg.attachments.size
-                if(att > 1){result = true}else{result = false}
-                 break;
-                 case 11:
-                  invite = new RegExp(/(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/.+[a-z]/g);
-                  if(invite.test(msg.content)){result = true}else{result = false}
-                   break;
+      case 7:
+        mentions = msg.mentions.users.size
+        if (mentions > 1) { result = true } else { result = false }
+        break;
+      case 8:
+        react = msg.reactions.cache.size
+        if (react > 0) { result = true } else { result = false }
+        break;
+      case 9:
+        att = msg.attachments.size
+        if (att == 1) { result = true } else { result = false }
+        break;
+      case 10:
+        att = msg.attachments.size
+        if (att > 1) { result = true } else { result = false }
+        break;
+      case 11:
+        invite = new RegExp(/(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/.+[a-z]/g);
+        if (invite.test(msg.content)) { result = true } else { result = false }
+        break;
+      case 12:
+        react = msg.embeds.length
+        if (react > 0) { result = true } else { result = false }
+        break;
+      case 13:
+        react = msg.embeds.length
+        if (react = 2) { result = true } else { result = false }
+        break;
       default:
         break;
     }
@@ -122,5 +176,5 @@ module.exports = {
     this.prepareActions(data.branch?.iffalseActions);
   },
 
-  mod() {},
+  mod() { },
 };
