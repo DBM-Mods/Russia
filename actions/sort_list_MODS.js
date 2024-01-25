6005cdb9b36ddf4bbb6b3745c21de5e680faa9db
LIK (DANIL) <61782941+likDanil@users.noreply.github.com>
2024-01-25 18:22:28 +0500

    Обновление действия [0.3]

--- actions/sort_list_MODS.js
+++ actions/sort_list_MODS.js
@@ -5,30 +5,50 @@ module.exports = {
 	meta: {
 		version: '2.1.7',
 		preciseCheck: true,
-		author: '[XinXyla - 172782058396057602]',
+		author: '[xinxyla - 172782058396057602]',
 		authorUrl: 'https://github.com/DBM-Mods/Russia',
 		downloadURL: 'https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip',
-	  },
-	
-	subtitle: function(data) {
-		const list = ['', 'Временная переменная', 'Серверная переменная', 'Глобальная переменная'];
-		const classic = ['По возрастанию [0-9]', 'По убыванию [9-0]', 'По алфавиту [A-Z]', 'По алфавиту в обратном порядке [Z-A]', 'По длине [по возрастанию]', 'По длине [по убыванию]'];
-		return `(${classic[parseInt(data.sorte)]}) ${list[parseInt(data.list)]} (${data.varName}) para ${list[parseInt(data.storage)]} (${data.varName2})`;
 	},
-	
-	
+
+
+	subtitle: function (data) {
+
+		if (data.descriptionx == true) {
+			desccor = data.descriptioncolor
+		} else {
+			desccor = 'none'
+		}
+
+		const list = ['', 'Временная переменная', 'Серверная переменная', 'Глобальная переменная '];
+		const classic = ['Порядок возрастания [0-9]', 'Порядок убыванния [9-0]', 'Алфавитный порядок [A-Z]', 'Обратный алфавитный порядок [Z-A]', 'Длина [по возрастанию]', 'Длина [в порядке убывания]'];
+
+		return data.description
+			? `<font style="color:${desccor}">${data.description}</font>`
+			: `<font style="color:${desccor}">(${classic[parseInt(data.sorte)]}) ${list[parseInt(data.list)]} (${data.varName}) para ${list[parseInt(data.storage)]} (${data.varName2})</font>`
+	},
+
+
 	variableStorage(data, varType) {
 		if (parseInt(data.storage, 10) !== varType) return;
-		return [data.varName2, 'Список'];
-	  },
-	
-	
-	fields: ["list", "varName", "storage", "varName2", "sorte"],
-	
-	html: function(isEvent, data) {
+		return [data.varName2, 'List'];
+	},
+
+
+	fields: ["list", "varName", "storage", "varName2", "sorte", "descriptioncolor", "description", "descriptionx"],
+
+	html: function (isEvent, data) {
 		return `
-	    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Версия 0.2</div>
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+		<div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Russia/archive/refs/heads/main.zip">Обновить</div>
+		<div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Russia">Версия 0.3</div>
+
+		<div style="width: 100%; padding:5px 5px;height: calc(100vh - 160px);overflow:auto">
+
+<table style="width:100%;"><tr>
+<td><span class="dbminputlabel">Описание действия</span><br><input type="text" class="round" id="description" placeholder="Не обязательное поле"></td>
+<td style="padding:0px 0px 0px 10px;width:70px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Цвет (вкл)"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
+</tr></table>
+<br>
+
 	<div>
 		<div style="float: left; width: 35%;">
 		<span class="dbminputlabel">Список</span><br>
@@ -41,20 +61,21 @@ module.exports = {
 			<input id="varName" class="round" type="text" list="variableList"><br>
 		</div><br><br><br>
 		<div style=" float: right; width: 100%;">
-		<span class="dbminputlabel">Сортировать список</span><br>
+		<span class="dbminputlabel">Сортировка списка</span><br>
 			<select id="sorte" class="round" style="width: 100%;">
-			<option value="0" selected>Сортировать числа по возрастанию [0-9]</option>
-            <option value="1">Сортировать числа по убыванию [9-0]</option>
-            <option value="2">Сортировать по алфавиту [A-Z]</option>
-            <option value="3">Сортировать по алфавиту в обратном порядке [Z-A]</option>
-            <option value="4">Сортировать по длине [по возрастанию]</option>
-            <option value="5">Сортировать по длине [по убыванию]</option>
+			<option value="0" selected>Сортировка чисел в порядке возрастания [0-9]</option>
+			<option value="1">Сортировка чисел в порядке убывания [9-0]</option>
+				<option value="2">Сортировать по алфавиту [A-Z]</option>
+				<option value="3">Сортировка в обратном алфавитном порядке [Z-A]</option>
+				<option value="4">Сортировать по длине [по возрастанию]</option>
+				<option value="5">Сортировать по длине [по убыванию]</option>
+				<option value="6">Перемешать</option>
 			</select>
 		</div>
 	</div><br><br><br><br>
 	<div>
 		<div style="float: left; width: 35%;">
-		<span class="dbminputlabel">Хранить в</span><br>
+		<span class="dbminputlabel">Сохранить в</span><br>
 			<select id="storage" class="round">
 				${data.variables[1]}
 			</select>
@@ -63,55 +84,91 @@ module.exports = {
 		<span class="dbminputlabel">Имя переменной</span><br>
 			<input id="varName2" class="round" type="text">
 		</div>
-	</div>`
+	</div>
+	
+
+	</div>
+	<style>
+
+.dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
+.dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
+
+</style>`
 	},
-	
-	init: function() {
-		const {glob, document} = this;
-	
+
+	init: function () {
+		const { glob, document } = this;
+
 		glob.refreshVariableList(document.getElementById('list'));
-	
+
+		const xinelaslinks = document.getElementsByClassName('xinelaslink');
+		for (let x = 0; x < xinelaslinks.length; x++) {
+			const xinelaslink = xinelaslinks[x];
+			const url = xinelaslink.getAttribute('data-url');
+			if (url) {
+				xinelaslink.setAttribute('title', url);
+				xinelaslink.addEventListener('click', (e) => {
+					e.stopImmediatePropagation();
+					console.log(`Launching URL: [${url}] in your default browser.`);
+					require('child_process').execSync(`start ${url}`);
+				});
+			}
+		}
+
 	},
-	
-	  action: function(cache) {
+
+	action: function (cache) {
 		const data = cache.actions[cache.index];
-		const storage = parseInt(data.storage);
+		const storage = parseInt(data.list);
 		const varName = this.evalMessage(data.varName, cache);
 		const list = this.getVariable(storage, varName, cache);
 		const sorte = parseInt(data.sorte);
 
-    let result;
-	
+		let result;
 
-		switch(sorte) {
+
+		switch (sorte) {
 			case 0:
-				result = list.sort((a ,b)=> a - b);
+				result = list.slice().sort((a, b) => a - b);
 				break;
 			case 1:
-				result = list.sort((a ,b)=> b - a);
+				result = list.slice().sort((a, b) => b - a);
 				break;
 			case 2:
-				result = list.sort();
-				break;				
+				result = list.slice().sort();
+				break;
 			case 3:
-				result = list.sort().reverse();
+				result = list.slice().sort().reverse();
 				break;
 			case 4:
-				result = list.sort(function(a, b){return a.length - b.length});
+				result = list.slice().sort(function (a, b) { return a.length - b.length });
 				break;
 			case 5:
-				result = list.sort(function(a, b){return b.length - a.length});
+				result = list.slice().sort(function (a, b) { return b.length - a.length });
+				break;
+			case 6:
+
+			const shuffledList = list.slice();
+
+			for (let i = shuffledList.length - 1; i > 0; i--) {
+			  const j = Math.floor(Math.random() * (i + 1));
+			  [shuffledList[i], shuffledList[j]] = [shuffledList[j], shuffledList[i]];
+			}
+
+
+				result = shuffledList
+
 				break;
 		}
-		
+
 		if (result !== undefined) {
-			const storage = parseInt(data.storage, 10);
+			const storage2 = parseInt(data.storage, 10);
 			const varName2 = this.evalMessage(data.varName2, cache);
-			this.storeValue(result, storage, varName2, cache);
+			this.storeValue(result, storage2, varName2, cache);
 		}
-	
+
 		this.callNextAction(cache);
 	},
-	
-		mod() {},
+
+	mod() { },
 };
